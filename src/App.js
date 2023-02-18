26d0e9031d9adc2ea15ba744441ab610d3c3ba83
Hashin Mohammed <hashinpks@gmail.com>
2023-02-19 00:00:29 +0530

    first image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from "./components/NavBar";
 
 function App() {
+  const imageUrl = 'https://tesla-cdn.thron.com/delivery/public/image/tesla/a269d7b4-87a9-4057-9ac5-2565e56a188e/bvlatuR/std/4096x2560/Homepage-Model-3-Desktop-LHD?quality=auto-medium&amp;format=auto';
+  const style = {
+    backgroundImage: `url(${imageUrl})`
+    
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="lg:bg h-screen bg-cover bg-center" style={style}>
+      
+   <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+        <img src="" alt="" />
+        <div>
+            <ul>
+                <li>Model S</li>
+                <li>Model 3</li>
+                <li>Model X</li>
+                <li>Model Y</li>
+                <li>Solar Roof</li>
+                <li>Solar Panels</li>
+            </ul>
+        </div>
+        <div>
+            <ul>
+                <li>Shop</li>
+                <li>Account</li>
+                <li>Menu</li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
