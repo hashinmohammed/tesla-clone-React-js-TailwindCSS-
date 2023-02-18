819dd1ce5392a38ab5f0873d6ab96fa80cbd6827
Hashin Mohammed <hashinpks@gmail.com>
2023-02-19 01:24:15 +0530

    button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import NavBar from "./components/NavBar";
+import Model from './components/Model';
 
 function App() {
   const imageUrl = 'https://tesla-cdn.thron.com/delivery/public/image/tesla/a269d7b4-87a9-4057-9ac5-2565e56a188e/bvlatuR/std/4096x2560/Homepage-Model-3-Desktop-LHD?quality=auto-medium&amp;format=auto';
@@ -11,6 +12,7 @@ function App() {
     <div className="lg:bg h-screen bg-cover bg-center" style={style}>
       
    <NavBar />
+  <Model />
     </div>
   );
 }
